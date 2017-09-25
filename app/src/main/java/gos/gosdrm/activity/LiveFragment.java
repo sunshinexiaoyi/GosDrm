1edf4b7d0fb2114762e097073f9e52aaa2e8e212
QXTX_GOSDOWS10 <1085466442@qq.com>
2017-09-25 16:55:24 +0800

    完成live部分的初步布局

--- app/src/main/java/gos/gosdrm/activity/LiveFragment.java
+++ app/src/main/java/gos/gosdrm/activity/LiveFragment.java
@@ -17,27 +17,27 @@ public class LiveFragment extends Fragment {
                              Bundle savedInstanceState) {
         Log.e("live的消息", "进入onCreate()方法");
 
+        View view = inflater.inflate(R.layout.live_main,container, false);
         //设置时间和日期
-        setTime(inflater, container);
+        //setTime(view, inflater, container);
         //获取节目列表
         //获取节目epg
         //设置焦点监听
         //播放指定节目
         //检测wlan的状态
-        //搜索框的实现
+        //搜索框的改良
 
-        return inflater.inflate(R.layout.live_main, container, false);
+        return view;
     }
 
     //设置时间日期
-    public void setTime(LayoutInflater inflater, ViewGroup container) {
-        View view = inflater.inflate(R.layout.live_main,container, false);
+    public void setTime(View view, LayoutInflater inflater, ViewGroup container) {
         TextClock time = (TextClock)view.findViewById(R.id.live_time);
         TextClock date = (TextClock)view.findViewById(R.id.live_date);
         TextClock week = (TextClock)view.findViewById(R.id.live_week);
         time.setFormat24Hour("hh:mm");//设置时间
-        date.setFormat24Hour("yyyy-MM-dd");//设置日期
-        date.setFormat24Hour("EEEE");//设置星期
+        date.setFormat24Hour("yyyy年MMMM月dd日");//设置日期
+        week.setFormat24Hour("EEEE");//设置星期
         Log.e("设置时间日期消息", "设置完成");
     }
 }
