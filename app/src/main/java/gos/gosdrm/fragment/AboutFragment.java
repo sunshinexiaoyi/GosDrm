4c4190512284c9c8737821f840a94bbd534b54e7
dujiakai <1090564068@qq.com>
2017-09-28 15:59:48 +0800

    添加了”关于“fragment

--- app/src/main/java/gos/gosdrm/fragment/AboutFragment.java
+++ app/src/main/java/gos/gosdrm/fragment/AboutFragment.java
@@ -7,103 +7,64 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import gos.gosdrm.R;
+import gos.gosdrm.adapter.ReuseAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AboutFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AboutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AboutFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private ListView mListview;
+    private View view;
+    ReuseAdapter<String> listAdapter;
 
     public AboutFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        view = inflater.inflate(R.layout.fragment_about, container, false);
+        initListview();
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    public void  initListview() {
+        mListview = view.findViewById(R.id.aus_listView);
+        listAdapter = new  ReuseAdapter<String>(getActivity(), R.layout.item_about_us) {
+            @Override
+            public void bindView(Holder holder, String obj) {
+                holder.setText(R.id.item_aus_tv, obj);
+            }
+        };
+        listAdapter.add(0, "功能介绍");
+        listAdapter.add(1, "投诉");
+        listAdapter.add(2, "检查新版本");
+        mListview.setAdapter(listAdapter);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        mListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        Toast.makeText(getActivity(), "爱奇艺影音是一款专注视频播放的客户端软件，热播大剧、最新剧集，八卦娱乐，奇艺在手，应有尽有。主要功能：\n" +
+                                "1、免费使用：免费安装，免费观看高清正版影视。\n" +
+                                "2、内容丰富：最新影视、最热综艺、旅游、纪录片，支持奇艺网全部内容。\n" +
+                                "3、播放流畅：比在线观看更流畅，越多人看越流畅。\n" +
+                                "4、独家功能：边选边看，全屏关灯，清晰度调节。", Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        Toast.makeText(getActivity(), "投诉不了", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(getActivity(), "已经是最新版本了", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 }
